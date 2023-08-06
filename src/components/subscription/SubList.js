6be25ce4aaccf1eca5a7277067612f510a6c4a04
keipy27 <kevinandris27@gmail.com>
2023-08-06 12:07:41 +1200

    styles for Sublist.js added

--- src/components/subscription/SubList.js
+++ src/components/subscription/SubList.js
@@ -5,15 +5,15 @@ import Sub from './Sub'
 const SubList = () => {
   return (
     <section className='main'>
-        <div className="container">
+        <div className="container --center-all">
 
             <div className="title">
                 <h2>Pricing</h2>
                 <div className="--line"></div>
 
-                <div>
+                <div className='--flex-center --my2'>
                     <p>Monthly</p>
-                    <div>
+                    <div className='--mx2'>
                         <span className="toggle-btn">
                             <div className="ball"></div>
                         </span>
@@ -23,6 +23,8 @@ const SubList = () => {
             </div>
 
             <div className="sub-plans">
+                <Sub />
+                <Sub />
                 <Sub />
             </div>
 
